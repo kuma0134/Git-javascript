1c75578763b33ce7f0ed9bb90e6735a9e2a3c806
Nitin Kumar <kuma0134@algonquinlive.com>
2019-09-10 12:53:07 -0400

    Changes to Javascript file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var myName = 'Nitin Kumar'
-console.log(`Hello my name is $(myName).`)
+var name = 'Nitin Kumar'
+console.log(`Hello my name is $(name).`)
 var number = 5
 console.log(number * 2 * 5 / number - 7)
